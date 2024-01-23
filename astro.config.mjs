fc1e8466931840a1a991179fce8fbd049f483dea
mabuabaras <84805226+mabuabaras@users.noreply.github.com>
2024-01-23 09:29:27 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  // renderers: ["@astrojs/renderer-react"],
-  integrations: [react()],
   output: "server",
+  // renderers: ["@astrojs/renderer-react"],
+  integrations: [react()]
 });
