b0d938cddfa9e0ce68e7e9385130bbaeeba80d68
mabuabaras <84805226+mabuabaras@users.noreply.github.com>
2024-01-23 09:36:10 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
-  output: "server",
   // renderers: ["@astrojs/renderer-react"],
   integrations: [react()],
+  output: "server",
   adapter: node({
     mode: "standalone"
   })
